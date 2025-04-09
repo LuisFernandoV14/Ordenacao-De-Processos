4ede84846624b2cb8b1b0209f96977dc62dbdb88
Luís Fernando Ventura Ferreira <venturaluisf7@outlook.com>
2025-04-09 00:01:08 -0300

    Update and rename teste-6.c to prototipo-1.c
    Agora que eu consegui adicionar os processos a uma struct troquei o nome de teste para protótipo.
    Eu também transformei a função de ler os processos e atribuir a uma struct em uma função separada da main
    Resolvi a primeira ordem 3. De ordenar por id_classe e contar quantos processos de um determinado id_classe. Para isso foram implementadas as funções "contarIDs_classeUnicos()" e "imprimirProcessoPorIdClasse()".

--- prototipo-1.c
+++ prototipo-1.c
@@ -32,171 +32,205 @@ typedef struct Processo {
 void moverCursor(FILE *arquivo, size_t BytesVoltados);
 void imprimirProcesso(FILE *arquivoAlvo, PROCESSO processoImpresso);
 void limparArrays(PROCESSO *processo);
+void atribuirProcessosAStruct(FILE *arquivoBase, PROCESSO *vetorDeProcessos, int numeroDeLinhas);
+void copiarProcesso(PROCESSO *p1, PROCESSO *p2);
+void contarIDs_classeUnicos(PROCESSO *vetorDeProcessos, int *vetorDeIDs_Unicos, int nmrDeLinhas, FILE *auxiliar);
+void imprimirProcessoPorIdClasse(FILE *arquivoAlvo, PROCESSO *listaDeProcessos, int *vetorDeIDs_Unicos, int nmrDeLinhas);
+
 
 int main(){
-    FILE *novoArquivo, *velhoArquivo;
+    FILE *novoArquivo, *velhoArquivo, *arquivoAuxiliar;
     char parametroDeLinha, charDif, header[100];
-    int indice = 0, nmrDeLinhas = 0;
+    int nmrDeLinhas = 0; 
 
     velhoArquivo = fopen("processo_043_202409032338.csv", "r");
     novoArquivo = fopen("arquivo de teste.csv", "w");
+    arquivoAuxiliar = fopen("arquivo-Auxiliar.csv", "w");
 
     if (velhoArquivo == NULL) {printf("Erro ao abrir o arquivo \"processo_043_202409032338.csv\".\n"); exit(1); }
-    if (novoArquivo == NULL) {printf("Erro ao abrir o arquivo \"teste.csv\"\n."); exit(1); }
+    if (novoArquivo == NULL) {printf("Erro ao abrir o arquivo \"teste.csv\".\n"); exit(1); }
+    if (arquivoAuxiliar == NULL) {printf("Erro ao abrir o arquivo \"arquivo-Auxiliar.csv\".\n"); exit(1); }
     
-    while((parametroDeLinha = fgetc(velhoArquivo)) != EOF) { if(parametroDeLinha == '\n') nmrDeLinhas++; } // percorre até o final do arquivo contando as linhas (\n)
-    rewind(velhoArquivo); // volta para o início do arquivo
-    fscanf(velhoArquivo, "%[^\n]\n", header); // armazena o cabeçalho
+    // percorre até o final do arquivo contando as linhas (\n)
+    while((parametroDeLinha = fgetc(velhoArquivo)) != EOF) { if(parametroDeLinha == '\n') nmrDeLinhas++; } 
 
+    // volta para o início do arquivo
+    rewind(velhoArquivo); 
+    // armazena o cabeçalho
+    fscanf(velhoArquivo, "%[^\n]\n", header); 
+
+    // Declara dinamicamente dois vetores, um para todos os processos e um para todos "id_classe" únicos
     PROCESSO *listaDeProcessos = (PROCESSO *) malloc (sizeof(PROCESSO) * nmrDeLinhas); 
+    int *vetorDeIDs_Unicos = (int *) calloc (nmrDeLinhas, sizeof(int));
+
 
     fprintf(novoArquivo, "%s\n", header);
 
-    while (indice < nmrDeLinhas) {
-        
-        // Inicializar arrays com zeros
-        limparArrays(&listaDeProcessos[indice]);
 
-        // Ler parte comum do registro
-        fscanf(velhoArquivo, "%d,\"%[^\"]\",%d-%d-%d %d:%d:%f,",
-            &listaDeProcessos[indice].id,
-            listaDeProcessos[indice].numero,
-            &listaDeProcessos[indice].ano,
-            &listaDeProcessos[indice].mes,
-            &listaDeProcessos[indice].dia,
-            &listaDeProcessos[indice].horas,
-            &listaDeProcessos[indice].minutos,
-            &listaDeProcessos[indice].segundos);
+    // Bota todos os processos em uma struct própria
+    atribuirProcessosAStruct(velhoArquivo, listaDeProcessos, nmrDeLinhas);
+    // Escreve todos os processos em um arquivo novo 
+    for (int i = 0; i < nmrDeLinhas; i++) {imprimirProcesso(novoArquivo, listaDeProcessos[i]);}
 
-        // Verificar se o próximo caractere é aspas ou chave
-        charDif = fgetc(velhoArquivo);
-        moverCursor(velhoArquivo, -1);
-        
-        if (charDif == '{') { // id_classe começa com {
-            char buffer[100];
-            // Verificar se é um único ID de classe ou múltiplos
-            fscanf(velhoArquivo, "{%[^}]}", buffer);
-            
-            // Processar o buffer para extrair os valores
-            char *token = strtok(buffer, ",");
-            int i = 0;
-            while (token != NULL && i < 5) {
-                listaDeProcessos[indice].id_classe[i] = atoi(token);
-                token = strtok(NULL, ",");
-                i++;
-            }
-            
-            // Ler o próximo caractere após }
-            charDif = fgetc(velhoArquivo);
-            moverCursor(velhoArquivo, -1);
-            
-            // Verificar se o próximo caractere é vírgula
-            if (charDif == ',') {
-                fgetc(velhoArquivo); // consumir a vírgula
-                
-                // Verificar o tipo de id_assunto
-                charDif = fgetc(velhoArquivo);
-                moverCursor(velhoArquivo, -1);
-                
-                if (charDif == '{') { // Formato: {id_assunto}
-                    fscanf(velhoArquivo, "{%[^}]},%d\n", buffer, &listaDeProcessos[indice].ano_eleicao);
-                    
-                    // Processar o buffer para extrair os valores
-                    token = strtok(buffer, ",");
-                    i = 0;
-                    while (token != NULL && i < 5) {
-                        listaDeProcessos[indice].id_assunto[i] = atoi(token);
-                        token = strtok(NULL, ",");
-                        i++;
-                    }
+    // Preenche o vetor de "id_classe" unicos e imprime em um arquivo auxiliar
+    contarIDs_classeUnicos(listaDeProcessos, vetorDeIDs_Unicos, nmrDeLinhas, arquivoAuxiliar);
+    printf("%d", vetorDeIDs_Unicos[0]);
+    for (int i = 1; vetorDeIDs_Unicos[i] > 0; i ++) { printf(", %d", vetorDeIDs_Unicos[i]);}
 
-                    listaDeProcessos[indice].tipoDeLinha = 1;
+    imprimirProcessoPorIdClasse(arquivoAuxiliar, listaDeProcessos, vetorDeIDs_Unicos, nmrDeLinhas);
 
 
-                } else if (charDif == '"') { // Formato: "{id_assunto}"
-                    fscanf(velhoArquivo, "\"{%[^}]}\",%d\n", buffer, &listaDeProcessos[indice].ano_eleicao);
-                    
-                    // Processar o buffer para extrair os valores
-                    token = strtok(buffer, ",");
-                    i = 0;
-                    while (token != NULL && i < 5) {
-                        listaDeProcessos[indice].id_assunto[i] = atoi(token);
-                        token = strtok(NULL, ",");
-                        i++;
-                    }
-
-                    listaDeProcessos[indice].tipoDeLinha = 2;
-
-                }
-            }
-        } else if (charDif == '"') { // id_classe começa com "
-            char buffer[100];
-            fscanf(velhoArquivo, "\"{%[^}]}\"", buffer);
-            
-            // Processar o buffer para extrair os valores de id_classe
-            char *token = strtok(buffer, ",");
-            int i = 0;
-            while (token != NULL && i < 5) {
-                listaDeProcessos[indice].id_classe[i] = atoi(token);
-                token = strtok(NULL, ",");
-                i++;
-            }
-            
-            // Ler o próximo caractere após "
-            charDif = fgetc(velhoArquivo);
-            
-            // Verificar se o próximo caractere é vírgula
-            if (charDif == ',') {
-                // Verificar o tipo de id_assunto
-                charDif = fgetc(velhoArquivo);
-                moverCursor(velhoArquivo, -1);
-                
-                if (charDif == '{') { // Formato: ,{id_assunto}
-                    fscanf(velhoArquivo, "{%[^}]},%d\n", buffer, &listaDeProcessos[indice].ano_eleicao);
-                    
-                    // Processar o buffer para extrair os valores
-                    token = strtok(buffer, ",");
-                    i = 0;
-                    while (token != NULL && i < 5) {
-                        listaDeProcessos[indice].id_assunto[i] = atoi(token);
-                        token = strtok(NULL, ",");
-                        i++;
-                    }
-
-                    listaDeProcessos[indice].tipoDeLinha = 3;
-
-                } else if (charDif == '"') { // Formato: ,"{id_assunto}"
-                    fscanf(velhoArquivo, "\"{%[^}]}\",%d\n", buffer, &listaDeProcessos[indice].ano_eleicao);
-                    
-                    // Processar o buffer para extrair os valores
-                    token = strtok(buffer, ",");
-                    i = 0;
-                    while (token != NULL && i < 5) {
-                        listaDeProcessos[indice].id_assunto[i] = atoi(token);
-                        token = strtok(NULL, ",");
-                        i++;
-                    }
-
-                    listaDeProcessos[indice].tipoDeLinha = 4;
-
-                }
-            }
-        }
-        
-        imprimirProcesso(novoArquivo, listaDeProcessos[indice]);
-        
-        indice++;
-    }
-
-    free(listaDeProcessos);
-    fclose(velhoArquivo);
-    fclose(novoArquivo);
+    // Evita vazamento de memória
+    free(listaDeProcessos); free(vetorDeIDs_Unicos);
+    fclose(arquivoAuxiliar); fclose(velhoArquivo); fclose(novoArquivo);
     
     return 0;
 }
 
 void moverCursor(FILE *arquivo, size_t BytesVoltados) { 
     fseek(arquivo, BytesVoltados, SEEK_CUR);
+}
+
+void atribuirProcessosAStruct(FILE *arquivoBase, PROCESSO *vetorDeProcessos, int numeroDeLinhas) {
+    
+int indice = 0;
+char charDif;
+
+while (indice < numeroDeLinhas) {
+
+        // Inicializar arrays com zeros
+        limparArrays(&vetorDeProcessos[indice]);
+
+        // Ler parte comum do registro
+        fscanf(arquivoBase, "%d,\"%[^\"]\",%d-%d-%d %d:%d:%f,",
+            &vetorDeProcessos[indice].id,
+            vetorDeProcessos[indice].numero,
+            &vetorDeProcessos[indice].ano,
+            &vetorDeProcessos[indice].mes,
+            &vetorDeProcessos[indice].dia,
+            &vetorDeProcessos[indice].horas,
+            &vetorDeProcessos[indice].minutos,
+            &vetorDeProcessos[indice].segundos);
+
+        // Verificar se o próximo caractere é aspas ou chave
+        charDif = fgetc(arquivoBase);
+        moverCursor(arquivoBase, -1);
+        
+        if (charDif == '{') { // id_classe começa com {
+            char buffer[100];
+            // Verificar se é um único ID de classe ou múltiplos
+            fscanf(arquivoBase, "{%[^}]}", buffer);
+            
+            // Processar o buffer para extrair os valores
+            char *token = strtok(buffer, ",");
+            int i = 0;
+            while (token != NULL && i < 5) {
+                vetorDeProcessos[indice].id_classe[i] = atoi(token);
+                token = strtok(NULL, ",");
+                i++;
+            }
+            
+            // Ler o próximo caractere após }
+            charDif = fgetc(arquivoBase);
+            moverCursor(arquivoBase, -1);
+            
+            // Verificar se o próximo caractere é vírgula
+            if (charDif == ',') {
+                fgetc(arquivoBase); // consumir a vírgula
+                
+                // Verificar o tipo de id_assunto
+                charDif = fgetc(arquivoBase);
+                moverCursor(arquivoBase, -1);
+                
+                if (charDif == '{') { // Formato: {id_assunto}
+                    fscanf(arquivoBase, "{%[^}]},%d\n", buffer, &vetorDeProcessos[indice].ano_eleicao);
+                    
+                    // Processar o buffer para extrair os valores
+                    token = strtok(buffer, ",");
+                    i = 0;
+                    while (token != NULL && i < 5) {
+                        vetorDeProcessos[indice].id_assunto[i] = atoi(token);
+                        token = strtok(NULL, ",");
+                        i++;
+                    }
+
+                    vetorDeProcessos[indice].tipoDeLinha = 1;
+
+
+                } else if (charDif == '"') { // Formato: "{id_assunto}"
+                    fscanf(arquivoBase, "\"{%[^}]}\",%d\n", buffer, &vetorDeProcessos[indice].ano_eleicao);
+                    
+                    // Processar o buffer para extrair os valores
+                    token = strtok(buffer, ",");
+                    i = 0;
+                    while (token != NULL && i < 5) {
+                        vetorDeProcessos[indice].id_assunto[i] = atoi(token);
+                        token = strtok(NULL, ",");
+                        i++;
+                    }
+
+                    vetorDeProcessos[indice].tipoDeLinha = 2;
+
+                }
+            }
+        } else if (charDif == '"') { // id_classe começa com "
+            char buffer[100];
+            fscanf(arquivoBase, "\"{%[^}]}\"", buffer);
+            
+            // Processar o buffer para extrair os valores de id_classe
+            char *token = strtok(buffer, ",");
+            int i = 0;
+            while (token != NULL && i < 5) {
+                vetorDeProcessos[indice].id_classe[i] = atoi(token);
+                token = strtok(NULL, ",");
+                i++;
+            }
+            
+            // Ler o próximo caractere após "
+            charDif = fgetc(arquivoBase);
+            
+            // Verificar se o próximo caractere é vírgula
+            if (charDif == ',') {
+                // Verificar o tipo de id_assunto
+                charDif = fgetc(arquivoBase);
+                moverCursor(arquivoBase, -1);
+                
+                if (charDif == '{') { // Formato: ,{id_assunto}
+                    fscanf(arquivoBase, "{%[^}]},%d\n", buffer, &vetorDeProcessos[indice].ano_eleicao);
+                    
+                    // Processar o buffer para extrair os valores
+                    token = strtok(buffer, ",");
+                    i = 0;
+                    while (token != NULL && i < 5) {
+                        vetorDeProcessos[indice].id_assunto[i] = atoi(token);
+                        token = strtok(NULL, ",");
+                        i++;
+                    }
+
+                    vetorDeProcessos[indice].tipoDeLinha = 3;
+
+                } else if (charDif == '"') { // Formato: ,"{id_assunto}"
+                    fscanf(arquivoBase, "\"{%[^}]}\",%d\n", buffer, &vetorDeProcessos[indice].ano_eleicao);
+                    
+                    // Processar o buffer para extrair os valores
+                    token = strtok(buffer, ",");
+                    i = 0;
+                    while (token != NULL && i < 5) {
+                        vetorDeProcessos[indice].id_assunto[i] = atoi(token);
+                        token = strtok(NULL, ",");
+                        i++;
+                    }
+
+                    vetorDeProcessos[indice].tipoDeLinha = 4;
+
+                }
+            }
+        }
+        
+        indice++;
+
+    }
+
 }
 
 void limparArrays(PROCESSO *processo) {
@@ -207,6 +241,7 @@ void limparArrays(PROCESSO *processo) {
 }
 
 void imprimirProcesso(FILE *arquivoAlvo, PROCESSO processoImpresso) {
+
     int i = 0;
     
     // Parte comum da formatação para todos os tipos de linha
@@ -287,5 +322,112 @@ void imprimirProcesso(FILE *arquivoAlvo, PROCESSO processoImpresso) {
 
                 limparArrays(&processoImpresso);
             break;
+    }
+}
+
+void copiarProcesso(PROCESSO *p1, PROCESSO *p2) {
+
+    int i = 0, j = 0;
+    
+    (*p2).id = (*p1).id;
+    strcpy((*p2).numero, (*p1).numero);
+    (*p2).mes = (*p1).mes;
+    (*p2).ano = (*p1).ano;
+    (*p2).dia = (*p1).dia;
+    (*p2).horas = (*p1).horas;
+    (*p2).minutos = (*p1).minutos;
+    (*p2).segundos = (*p1).segundos;
+    (*p2).ano_eleicao = (*p1).ano_eleicao;
+    (*p2).tipoDeLinha = (*p1).tipoDeLinha;
+
+    
+    while ((*p1).id_classe[i] > 0 ) {
+
+        (*p2).id_classe[i] = (*p1).id_classe[i];
+        i++;
+    }
+
+    while ((*p1).id_assunto[j] > 0 ) {
+
+        (*p2).id_assunto[j] = (*p1).id_assunto[j];
+        j++;
+    }
+
+}
+
+void contarIDs_classeUnicos(PROCESSO *vetorDeProcessos, int *vetorDeIDs_Unicos, int nmrDeLinhas, FILE *auxiliar) {
+
+    static int limiteInteracoes = 0;
+    int indice = 0, k, l, seraSeRepete, enderecoVetor;
+
+
+        while (indice < nmrDeLinhas){
+
+            k = 0, l = 0, seraSeRepete = 1, enderecoVetor = 0;
+
+            while( vetorDeProcessos[indice].id_classe[k] > 0 ) {
+
+                while( l < limiteInteracoes) {
+
+                    if(  vetorDeProcessos[indice].id_classe[k] == vetorDeIDs_Unicos[enderecoVetor]) {
+
+                        seraSeRepete = 0;
+
+                    }
+
+                    enderecoVetor++;
+                    l++;
+                }
+
+                if (seraSeRepete > 0) {
+
+
+                    //fprintf(auxiliar, "Linha: %d, ID: %d\n", indice + 1,vetorDeProcessos[indice].id_classe[k]);
+                    vetorDeIDs_Unicos[limiteInteracoes] = vetorDeProcessos[indice].id_classe[k];
+                    limiteInteracoes = limiteInteracoes + 1;
+
+
+                }
+
+                k++;
+
+            }
+
+            indice++;
+
+        }
+
+
+
+}
+
+void imprimirProcessoPorIdClasse(FILE *arquivoAlvo, PROCESSO *listaDeProcessos, int *vetorDeIDs_Unicos, int nmrDeLinhas) {
+    int j = 0, contadorDeProcessos = 0;;
+    
+    while (vetorDeIDs_Unicos[j] > 0) {
+        int id_atual = vetorDeIDs_Unicos[j];
+        contadorDeProcessos = 0;
+        
+        if(j == 13) {j++; continue;}
+        
+        fprintf(arquivoAlvo, "--------------------------------------------------------------------------------\n");
+        fprintf(arquivoAlvo, "\n\tProcessos com ID_Classe {%d}:\t\n\n", id_atual);
+        
+        for (int i = 0; i < nmrDeLinhas; i++) {
+            int encontrado = 0;
+            
+            for (int k = 0; listaDeProcessos[i].id_classe[k] > 0; k++) {
+                if (listaDeProcessos[i].id_classe[k] == id_atual) {
+                    imprimirProcesso(arquivoAlvo, listaDeProcessos[i]);
+                    contadorDeProcessos++;
+                    encontrado = 1;
+                    break;
+                }
+            }
+        }
+        j++;
+
+        fprintf(arquivoAlvo, "\n\tQuantidade de processos com ID_Classe {%d}: %d\t\n\n", id_atual, contadorDeProcessos);
+        fprintf(arquivoAlvo, "--------------------------------------------------------------------------------\n");
     }
 }
